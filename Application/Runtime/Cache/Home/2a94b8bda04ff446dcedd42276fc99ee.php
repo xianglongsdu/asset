57a0722a54becd1a5e463b5f150417236141bb5a
xianglongsdu <xianglongsdu@163.com>
2016-12-09 23:10:11 +0800

    Version 0

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,20 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+<title>光大银行资产管理系统</title>
+<script type="text/javascript" src="/guangda/Public/easyui/jquery.min.js"></script>
+<script type="text/javascript" src="/guangda/Public/easyui/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="/guangda/Public/Home/js/home.js"></script>
+<link rel="stylesheet" type="text/css" href="/guangda/Public/easyui/themes/default/easyui.css">
+<link rel="stylesheet" type="text/css" href="/guangda/Public/easyui/themes/icon.css">
+</head>
+<body>
+<div id="cc" class="easyui-layout" style="width:80%;height:100%;margin: 0 auto;">
+    <div data-options="region:'north',title:'North Title',split:true" style="height:100px;"></div>
+    <div data-options="region:'south',title:'South Title',split:true" style="height:100px;"></div>
+    <div data-options="region:'east',title:'East',split:true" style="width:100px;"></div>
+    <div data-options="region:'west',title:'West',split:true" style="width:100px;"></div>
+    <div data-options="region:'center',title:'center title'" style="padding:5px;background:#eee;"></div>
+</div>
+</body>
+</html>
